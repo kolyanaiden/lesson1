c94f3ae320cb7179d82e4c48a31f3c723a8b921f
KolyaNaiden <foom.2223@mail.ru>
2022-06-26 18:13:42 +0300

    4

--- Program.cs
+++ Program.cs
@@ -39,23 +39,25 @@ else
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Input first number: ");
+/* Console.Write("Input first number: ");
 int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int number_2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int number_3 = Convert.ToInt32(Console.ReadLine());
 
-if (number_1 > number_2)
+if (number_1 > number_2 && number_1 > number_3)
 {
-    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_1);
-
+    Console.WriteLine(number_1 + ", " + number_2 + ", " + number_3 + " -> " + number_1);
+}
+if (number_2 > number_3)
+{
+    Console.WriteLine(number_1 + ", " + number_2 + ", " + number_3 + " -> " + number_2);
 }
 else
 {
-    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_2);
-
+    Console.WriteLine(number_1 + ", " + number_2 + ", " + number_3 + " -> " + number_3);
 }
 
 
-12312312342134231
+*/
