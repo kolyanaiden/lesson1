88be33ccf36d14ead9e9f503c841e178340eef84
KolyaNaiden <foom.2223@mail.ru>
2022-06-26 17:45:45 +0300

    2

--- Program.cs
+++ Program.cs
@@ -36,3 +36,23 @@ else
     Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_2);
 
 } */
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_1);
+
+}
+else
+{
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_2);
+
+}
