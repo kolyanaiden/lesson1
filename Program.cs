4d555f51b48950f7dbd0bb3f6fe3f49f3ceb82e5
KolyaNaiden <foom.2223@mail.ru>
2022-06-26 17:38:58 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int q = number * number;
+// Console.WriteLine(number + " -> " + q);
+
+// Console.Write("Input first number: ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number_1 == number_2 * number_2)
+// {
+//     Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " yes");
+
+// }
+// else
+// {
+//     Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " no");
+
+// }
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/* Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_1);
+
+}
+else
+{
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max " + number_2);
+
+} */
